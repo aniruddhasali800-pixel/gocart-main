601e8d5284ae95a34e3a440da0716269a99ae75f
Aniruddha <your-email@example.com>
2026-06-12 14:46:18 +0530

    Add sitemap and robots.txt for Google Search Console

---
+++ app/sitemap.js
@@ -0,0 +1,11 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://binarycomputers.shop',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    // You can dynamically map other routes like products here
+  ]
+}
