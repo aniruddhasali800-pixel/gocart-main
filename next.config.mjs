c7fb3834c45f75378d06eeabcdaf15f3e52e2fe3
Aniruddha <your-email@example.com>
2026-06-24 12:24:03 +0530

    Fix: Embed Clerk keys in next.config.mjs to bypass Vercel missing env error

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
     images:{
         unoptimized: true
+    },
+    env: {
+        CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY || "sk_test_aDTvGHXAx0llwqHbDJ0DNX9FC4JpXvlFCMTNPZc3Yz",
+        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || "pk_test_ZGVjaWRpbmctZG9nZmlzaC00MC5jbGVyay5hY2NvdW50cy5kZXYk",
     }
 };
 
